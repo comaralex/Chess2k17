1fee0466d9a1f2d679e8d5152392d78810d5acb7
Komarov Aleksei <alexcomarchim96@mail.ru>
2017-05-19 05:51:11 +0300

    Add toolButtons of figures to Options

--- chess/src/widgets/options.cpp
+++ chess/src/widgets/options.cpp
@@ -12,3 +12,24 @@ Options::~Options()
 {
     delete ui;
 }
+
+void Options::on_checkBoxBlackWhite_toggled(bool checked)
+{
+    if ( checked ) {
+        ui->checkBoxBlackWhite->setText("Белые/Черные");
+        ui->toolButtonPawn->setStyleSheet("border-image: url(:/res/images/images/pawn_white.svg);");
+        ui->toolButtonRook->setStyleSheet("border-image: url(:/res/images/images/rook_white.svg);");
+        ui->toolButtonKnight->setStyleSheet("border-image: url(:/res/images/images/knight_white.svg);");
+        ui->toolButtonBishop->setStyleSheet("border-image: url(:/res/images/images/bishop_white.svg);");
+        ui->toolButtonKing->setStyleSheet("border-image: url(:/res/images/images/king_white.svg);");
+        ui->toolButtonQueen->setStyleSheet("border-image: url(:/res/images/images/queen_white.svg);");
+    } else {
+        ui->checkBoxBlackWhite->setText("Черные/Белые");
+        ui->toolButtonPawn->setStyleSheet("border-image: url(:/res/images/images/pawn_black.svg);");
+        ui->toolButtonRook->setStyleSheet("border-image: url(:/res/images/images/rook_black.svg);");
+        ui->toolButtonKnight->setStyleSheet("border-image: url(:/res/images/images/knight_black.svg);");
+        ui->toolButtonBishop->setStyleSheet("border-image: url(:/res/images/images/bishop_black.svg);");
+        ui->toolButtonKing->setStyleSheet("border-image: url(:/res/images/images/king_black.svg);");
+        ui->toolButtonQueen->setStyleSheet("border-image: url(:/res/images/images/queen_black.svg);");
+    }
+}
--- chess/src/widgets/options.h
+++ chess/src/widgets/options.h
@@ -15,6 +15,9 @@ public:
     explicit Options(QWidget *parent = 0);
     ~Options();
 
+private slots:
+    void on_checkBoxBlackWhite_toggled(bool checked);
+
 private:
     Ui::FormOptions *ui;
 };
